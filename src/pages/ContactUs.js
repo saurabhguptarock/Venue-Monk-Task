2692fe42c3b9eade4f936b1c5c585907dc0b92f6
saurabh gupta <saurabhguptarock119@gmail.com>
2020-11-28 16:19:05 +0530

    added react modal and fixed things

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 const ContactUs = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = (isCoffee) => {
+    setShowModal(true);
+  };
+
   return (
     <div className="contact-container">
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+      ></Modal>
       <div className="contact-text">CONTACT US</div>
       <div className="contact-section">
-        <img className="contact" src="/24320.jpg" alt="Bar" />
-        <button className="button">Meet us at a Bar</button>
+        <img className="contact" src="/coffee.webp" alt="Bar" />
+        <button onClick={() => handleShowModal(true)} className="button">
+          Meet us at Coffee
+        </button>
       </div>
       <div className="contact-divider"></div>
       <div className="contact-section">
         <img className="contact" src="/4220501.jpg" alt="Office" />
-        <button className="button">Come to our Office</button>
+        <button onClick={() => handleShowModal(false)} className="button">
+          Come to our Office
+        </button>
       </div>
     </div>
   );
